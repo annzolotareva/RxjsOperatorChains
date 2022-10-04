184ecf542c736f8203b8fa93c3faf052aa6e6cbd
Золотарева Анна Александровна <aazolotareva@phoenixit.ru>
2022-10-04 16:47:07 +0300

    Новые изменения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { filter, from, map, Observable, of } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,30 @@ import { filter, from, map, Observable, of } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  email: string = '';
+  
+  @ViewChild('test', { static: true }) input: ElementRef | undefined; 
+
+
   title = 'RxjsOperatorChains';
+
+
   ngOnInit(): void {
+    fromEvent(this.input?.nativeElement, 'input')
+    .pipe(
+      map(val => this.input?.nativeElement.value))
+      .subscribe(console.log);
+    
+    //const baseUrl = 'https://swapi.dev/api/people';
+    //this.http.get(baseUrl)
+    //.pipe()
+
+
     from([10, 20, 40 , 41, 60])
   .pipe(
     map((num) => num * 2),
     filter((num) => num % 10 === 0),
+    
   )
   .subscribe((vl) => console.log(vl));
   }
